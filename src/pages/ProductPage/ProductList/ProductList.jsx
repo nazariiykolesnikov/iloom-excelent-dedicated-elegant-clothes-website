58895db93891e352d98621dc9649089183498441
Nazar Kolesnikov <nazarkolesnikov148@ukr.net>
2025-11-21 13:28:46 +0200

    first commit

---
+++ src/pages/ProductPage/ProductList/ProductList.jsx
@@ -0,0 +1,66 @@
+import "./ProductList.css";
+import "../ProductPage.css";
+import { assets } from "../../../assets/data/assets.js";
+
+const ProductList = ({ products }) => {
+    return (
+        <div className="product-list">
+            {products.map((product) => (
+                <div
+                    key={product.id}
+                    className={`product-card__container ${product.highlighted ? "highlighted" : ""}`}
+                >
+                    <img
+                        src={assets.filled_heart}
+                        alt="Favorite"
+                        className="product-card__filled_heart-icon"
+                    />
+                    <img
+                        src={assets.white_circle}
+                        alt="White circle"
+                        className="product-card__white_heart-icon"
+                    />
+                    <img
+                        src={product.image ?? assets.placeholder}
+                        alt={product.title}
+                        className="product-card__image"
+                    />
+                    <div className="product-card__content">
+            <span
+                className={`product-card__new-price ${
+                    product.priceWithWBWalletWithDiscount ? "discounted" : "normal"
+                }`}
+            >
+              {product.priceWithWBWalletWithDiscount} ₽
+            </span>
+                        {product.priceWithoutWBWalletAndWithoutDiscount && (
+                            <span className="product-card__old-price">
+                {product.priceWithoutWBWalletAndWithoutDiscount.toLocaleString("ru-RU")} ₽
+              </span>
+                        )}
+                    </div>
+                    <p className="product-card__product-name">{product.title}</p>
+                    <div className="product-card__rating-section">
+                        <img
+                            src={assets.star}
+                            alt="Rating"
+                            className="product-card__rating-icon"
+                        />
+                        <span className="product-card__product-rating">{product.rating}&nbsp;</span>
+                        <span className="product-card__product-mark">· {product.reviews} оценка</span>
+                    </div>
+                    <button className="product-card__add-to-card-button">
+                        <img
+                            src={assets.shopping_cart}
+                            alt="Shopping Cart"
+                            className="product-card__shopping-card-icon"
+                        />
+                        Добавить в корзину
+                    </button>
+                </div>
+            ))}
+        </div>
+    );
+};
+
+export default ProductList;
